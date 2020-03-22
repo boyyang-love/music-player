730d0b59212ac38db5f15486a5f65af16a7a8c1a
boyyang-love <boyyang-love@outlook.com>
2020-03-22 11:27:15 +0800

    提交第一分支

---
+++ vue-app/src/views/New.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="new"></div>
+</template>
+
+<script>
+export default {
+  name: "New"
+};
+</script>
+
+<style scoped>
+.new {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+</style>
